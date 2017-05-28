421bbf2f97dfc15b442538fd1062fda449183e07
Raul Pineda <raul@raulpineda.mx>
2017-05-28 20:46:42 +0200

    Add basic functionality

--- src/index.js
+++ src/index.js
@@ -1,7 +1,72 @@
 import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/Main';
+import TokenizedSearch from './components/TokenizedSearch';
 
-// Render the main component into the dom
-ReactDOM.render(<App />, document.getElementById('app'));
+require('styles/application.css');
+
+const defaultSearch = (query) => {
+  window.location.href = `https://testing.vivino.com/search?q=${query}`;
+}
+
+const searchGrapes = (query) => {
+  window.location.href = `https://testing.vivino.com/explore?grape_ids[]=${query}`;
+}
+
+const searchCountries = (query) => {
+  window.location.href = `https://testing.vivino.com/explore?country_codes[]=${query}`;
+}
+
+const searchFoods = (query) => {
+  window.location.href = `https://testing.vivino.com/explore?food_ids[]=${query}`;
+}
+
+const searchStyles = (query) => {
+  window.location.href = `https://testing.vivino.com/explore?wine_style_ids[]=${query}`;
+}
+
+const sources = [
+  {
+    id: 'grapes',
+    source: 'public/grapes.json',
+    iconPath: 'images/grape.png',
+    action: searchGrapes,
+    targetString: 'Search grapes',
+    uniqueKey: 'id',
+    labelKey: 'name'
+  },
+  {
+    id: 'countries',
+    source: 'public/countries.json',
+    iconPath: 'images/map.png',
+    action: searchCountries,
+    targetString: 'Search countries',
+    uniqueKey: 'code',
+    labelKey: 'name'
+  },
+  {
+    id: 'foods',
+    source: 'public/foods.json',
+    iconPath: 'images/food.png',
+    action: searchFoods,
+    targetString: 'Search foodpairings',
+    uniqueKey: 'id',
+    labelKey: 'name'
+  },
+  {
+    id: 'styles',
+    source: 'public/styles.json',
+    iconPath: 'images/style.png',
+    action: searchStyles,
+    targetString: 'Search styles',
+    uniqueKey: 'id',
+    labelKey: 'name'
+  }
+]
+
+ReactDOM.render(
+  <TokenizedSearch
+    sources={sources}
+    defaultSearch={defaultSearch} />,
+  document.getElementById('app')
+);
